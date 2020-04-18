b5ef6bfd6ee15e62630fa9e91d13129fea19f8d3
aferguson9806 <aferguson9806@gmail.com>
2020-04-17 21:50:41 -0400

    download function
    allows the user to download the file they created in the piano file

--- songgen_piano.py
+++ songgen_piano.py
@@ -1,6 +1,7 @@
 import winsound
 import random
 import time
+import wave
 
 
 class PianoSong:
@@ -38,33 +39,53 @@ class PianoSong:
         self.gsharpminor = [PianoSong.g5sharp, PianoSong.a5sharp, PianoSong.b5, PianoSong.c5sharp, PianoSong.d5sharp, PianoSong.e5, PianoSong.f5sharp]
         self.asharpminor = [PianoSong.a5sharp, PianoSong.c5, PianoSong.c5sharp, PianoSong.d5sharp, PianoSong.f5, PianoSong.f5sharp, PianoSong.g5sharp]
         
+        
+        #LIST TO BE TURNED INTO FILE
+        self.wavfiles = []
+        
+        #SONG NAME
+        
+        self.songname = 'yoursong'
 
     def c5(self):
         winsound.PlaySound("pianolowc", winsound.SND_ASYNC)
+        return "pianolowc.wav"
     def c5sharp(self):
         winsound.PlaySound("pianoc#", winsound.SND_ASYNC)
+        return "pianoc#.wav"
     def d5(self):
         winsound.PlaySound("pianod", winsound.SND_ASYNC)
+        return "pianod.wav"
     def d5sharp(self):
         winsound.PlaySound("pianod#", winsound.SND_ASYNC)
+        return "pianod#.wav"
     def e5(self):
         winsound.PlaySound("pianoe", winsound.SND_ASYNC)
+        return "pianoe.wav"
     def f5(self):
         winsound.PlaySound("pianof", winsound.SND_ASYNC)
+        return "pianof.wav"
     def f5sharp(self):
         winsound.PlaySound("pianof#", winsound.SND_ASYNC)
+        return "pianof#.wav"
     def g5(self):
         winsound.PlaySound("pianog", winsound.SND_ASYNC)
+        return "pianog.wav"
     def g5sharp(self):
         winsound.PlaySound("pianog#", winsound.SND_ASYNC)
+        return "pianog#.wav"
     def a5(self):
         winsound.PlaySound("pianoa", winsound.SND_ASYNC)
+        return "pianoa.wav"
     def a5sharp(self):
         winsound.PlaySound("pianoa#", winsound.SND_ASYNC)
+        return "pianoa#.wav"
     def b5(self):
         winsound.PlaySound("pianob", winsound.SND_ASYNC)
+        return "pianob.wav"
     def c6(self):
         winsound.PlaySound("pianohighc", winsound.SND_ASYNC)
+        return "pianohighc.wav"
 
 
     def define_song(self):
@@ -91,15 +112,36 @@ class PianoSong:
                 self.key += inputkey
             else: 
                 print("Invalid Key")
+                
+                
+        self.songname = input("Give your song a name!\n")
         
         
     def play_song(self):
         elapsed = 0
         self.key = eval(self.key)
         while elapsed < self.songinmilli:
-            random.choice(self.key)(mysong)
+            choice = random.choice(self.key)(mysong)
+            self.wavfiles.append(choice)
             time.sleep(2)
             elapsed += 2000
+            
+            
+    def compile_song(self):
+        
+        infiles = self.wavfiles
+        outfile = f"compiledsongs/{self.songname}.wav"
+        
+        with wave.open(outfile, 'wb') as wav_out:
+            for wav_path in infiles:
+                with wave.open(wav_path, 'rb') as wav_in:
+                    if not wav_out.getnframes():
+                        wav_out.setparams(wav_in.getparams())                              #this line shows an error for some reason but it works anyway
+                    wav_out.writeframes(wav_in.readframes(wav_in.getnframes()))            #this line too
+
+        
+
+        
         
         
         
@@ -110,3 +152,13 @@ if __name__ == "__main__":
     mysong.define_song()
 
     mysong.play_song()
+    
+    answersatisfy = False
+    
+    while answersatisfy == False:
+        answer = input("Would you like to download your song?\n")
+        if answer.lower() in ['y', 'n']:
+            answersatisfy = True
+            
+    if answer.lower() == 'y':
+        mysong.compile_song()
